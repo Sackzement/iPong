aa437c1dec88c2cd572764f05c9480e4d63b2b20
Pitri-Mac <pjoetri@gmail.com>
2015-11-30 04:40:54 +0100

    ulu

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@
 
 SDL_Window* window;
 SDL_Renderer* renderer;
-Uint32 time;
+Uint32 ticks;
 
 int main(int argc, char** argv)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	window = SDL_CreateWindow("iPong", 100, 100, 750, 1334, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("iPong", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 750, 1334, SDL_WINDOW_OPENGL);
+    SDL_FillRect(SDL_GetWindowSurface(window), NULL, 0);
+    SDL_UpdateWindowSurface(window);
 
-	while (time < 3000)
+	while (ticks < 5000)
 	{
-		time = SDL_GetTicks();
-		SDL_Delay(500);
+		ticks = SDL_GetTicks();
+		SDL_Delay(100);
 	}
 
 	SDL_DestroyWindow(window);
