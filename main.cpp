7c67fa49d403a0472d36295d4a91ec45dcff452b
Pitri-PC <pjoetri@gmail.com>
2015-12-28 07:16:55 +0100

    ulu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 
 #include <iostream>
 #include <Ngin/Ngin.h>
+#include <iostream>
+
 
 SDL_Window* window;
 SDL_Renderer* renderer;
@@ -8,6 +10,8 @@ Uint32 ticks;
 
 int main(int argc, char** argv)
 {
+	std::cout << sizeof(void*) << std::endl;
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	window = SDL_CreateWindow("iPong", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 750, 1334, SDL_WINDOW_OPENGL);
     SDL_FillRect(SDL_GetWindowSurface(window), NULL, 0);
